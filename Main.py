c27026524e5f87d4c146f20f208c79f53b499b8d
brian <brian@branwidth.com>
2017-04-26 00:05:02 +0000

    First endpoint for ranks
    Created /rank

--- Main.py
+++ Main.py
@@ -4,40 +4,43 @@ from slacker import Slacker
 from flask import Flask, request, Response
 import os
 
-ASSETS_DIR = os.path.dirname(os.path.abspath())
-
 app = Flask(__name__)
 
-SLACK_WEBHOOK_SECRET = "ULs9LTwlPrVTUP8CkTZYPuH1"
+SLACK_WEBHOOK_SECRET = os.environ.get("SlackWebSecret")
 
-slack = Slacker("xoxb-171642061520-h7f1IZCj3b69JxL7JagsjMfN")
+# Set some variables that we store in our .bashrc file on the server
+slack = Slacker(os.environ.get("SlackerKey"))
 riotapi.set_region("NA")
-riotapi.set_api_key("RGAPI-84f4a9e5-b6fd-493e-9786-0ccfde1147d4")
+riotapi.set_api_key(os.environ.get("RiotApiKey"))
 
-summoner = riotapi.get_summoner_by_name("TheBranHammer")
-# print("{name} is a level {level} summoner on the NA server.".format(name=summoner.name, level=summoner.level))
-
-# Send a message to #general channel
-# slack.chat.post_message('#league', 'Hello summoners!')
-
+# Setting a send message function to keep the app more modular
 def send_message(channel_name, message):
     slack.chat.post_message(channel_name, message)
 
-@app.route('/postgame', methods=['POST'])
-def inbound():
+# Command to get the rank of a champion.
+# Sends back the following:
+# Summoner name
+# Rank
+# LP
+@app.route('/rank', methods=['POST'])
+def rank():
     if request.form.get('token') == SLACK_WEBHOOK_SECRET:
         channel = request.form.get('channel_name')
-        username = request.form.get('user_name')
         text = request.form.get('text')
-        inbound_message = username + " in " + channel + "says: " + text
-        print(inbound_message)
-    return Response(), 200
+        summoner = riotapi.get_summoner_by_name(text)
+        rank = riotapi.get_league_entries_by_summoner(summoner)
+        rankInfo = [rank[0].tier, rank[0].entries[0].division, rank[0].entries[0].league_points]
+        message = str(text) + " is currently " + str(rankInfo[0])[5:].capitalize() + " " + str(rankInfo[1])[9:].capitalize() + " with " + str(rankInfo[2]) + " lp."
+        send_message(channel,message)
+    return Response(),200
 
+# Route for handling accidental Web HTTP requests to the flask server
 @app.route('/', methods=['GET'])
 def test():
-    return Response('It works!')
+    return Response('Hm... you should go here instead: www.branwidth.com')
     print("it worked")
 
+# Setting SSL values and running application
 if __name__ == "__main__":
-    context = ()
-    app.run(debug=True, host='0.0.0.0')
+    context = (os.environ.get("SSLCert"),os.environ.get("SSLPrivKey"))
+    app.run(debug=True, host='0.0.0.0',ssl_context=context)
